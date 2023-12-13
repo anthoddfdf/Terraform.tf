405eab26a7ad7630733f2b972cdcd48243f22950
Masterdev1@gmail.com <Masterdev1@gmail.com>
2023-12-13 11:04:57 +0000

    added test.tf

---
+++ test.tf
@@ -0,0 +1,29 @@
+#configuring provider
+
+provider "aws" {
+    region = "us-east-1"
+#    access_key = "access_key"
+#    secret_key = "secret_key"
+}
+
+
+#Deploy the test ec2 instance
+
+resource"aws_instance" "terraform-instance1" {
+         ami = "ami-0230bd60aa48260c6"
+         instance_type = "t1.micro"
+         tags = {
+         Name = "Jenkins"
+         }
+  
+}
+
+
+
+
+
+
+
+
+
+
